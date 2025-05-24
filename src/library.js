afd7c3c88e42cd1f888970cd608940a7542efa9c
LewdLeah <daisy.anais.197@gmail.com>
2025-05-24 18:31:36 -0400

    Delete src/library.js

--- src/library.js
+++
@@ -1,5 +0,0 @@
-/// <reference types="./types/global.d.ts" />
-// Your "Library" tab should look like this
-
-// [source code not public yet]
-// Estimated release date is May 26, 2025 (but no promises)
