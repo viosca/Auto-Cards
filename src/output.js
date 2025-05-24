9d0258b536d4698ea78ae18b59a8316a027a787f
LewdLeah <daisy.anais.197@gmail.com>
2025-05-24 18:34:36 -0400

    Update output.js

--- src/output.js
+++ src/output.js
@@ -1,1 +1,10 @@
-
+// Your "Output" tab should look like this
+const modifier = (text) => {
+  //
+  // Ideally, your other output code goes above this line
+  text = AutoCards("output", text);
+  // Alternatively, you may also put it below this line
+  //
+  return {text};
+};
+modifier(text);
