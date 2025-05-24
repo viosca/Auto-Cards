433ac88e6583dd58960370d7b845d72d3438f0b3
LewdLeah <daisy.anais.197@gmail.com>
2025-05-24 18:27:36 -0400

    Create library.js

---
+++ ts-src/library.js
@@ -0,0 +1,5 @@
+/// <reference types="./types/global.d.ts" />
+// Your "Library" tab should look like this
+
+// [source code not public yet]
+// Estimated release date is May 26, 2025 (but no promises)
